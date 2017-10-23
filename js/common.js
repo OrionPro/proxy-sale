2ef1781f8e542fb6159586e4169c96e58e2e24b4
orionpro <orion79@rambler.ru>
2017-10-24 02:59:53 +0300

    3 00 24.10.17

--- js/common.js
+++ js/common.js
@@ -1,4 +1,3 @@
-
 // подключение common.js
 
 // табы tabs
@@ -6,18 +5,18 @@ function tabs(obj) {
 	const buttons = document.querySelectorAll(obj.btn);
 	const bodyTabs = document.querySelectorAll(obj.tabsBody);
 
-	let func = function(){
+	let func = function () {
 		"use strict";
-		for( let i = buttons.length; i--; ){
+		for (let i = buttons.length; i--;) {
 			buttons[i].classList.remove(obj.classBtn);
 			bodyTabs[i].classList.remove(obj.classBody);
 		}
 		this.classList.add(obj.classBtn);
-		let item = [].indexOf.call(buttons,this);
+		let item = [].indexOf.call(buttons, this);
 		bodyTabs[item].classList.add(obj.classBody)
 	};
 
-	[].forEach.call(buttons,item => item.addEventListener('click',func));
+	[].forEach.call(buttons, item => item.addEventListener('click', func));
 }
 
 // Определения браузера
@@ -38,9 +37,9 @@ function get_name_browser() {
 	return 'Не определен';
 }
 
-$(document).ready( function() {
+$(document).ready(function () {
 
-	$('.dropdown-menu a').click(function(e){
+	$('.dropdown-menu a').click(function (e) {
 		e.preventDefault();
 		var self = $(this),
 			value = self.attr('rel') || false,
@@ -50,7 +49,7 @@ $(document).ready( function() {
 		dropdown.text(self.text()).data('value', value).triggerHandler('change');
 	});
 
-	$(".dropdown-menu li a").click(function(){
+	$(".dropdown-menu li a").click(function () {
 		$(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
 		$(this).parents(".dropdown").find('.btn').val($(this).data('value'));
 	});
@@ -65,20 +64,20 @@ $(document).ready( function() {
 		max: 8,
 		step: 1,
 		range: "min",
-		slide: function(event, ui) {
-			switch(ui.value){
+		slide: function (event, ui) {
+			switch (ui.value) {
 				case 2:
-				$("#amount").val(ui.value + " ядра по 2 ГГц");
-				$("#amount-span").text("ядра");
-				break;
+					$("#amount").val(ui.value + " ядра по 2 ГГц");
+					$("#amount-span").text("ядра");
+					break;
 				case 3:
-				$("#amount").val(ui.value + " ядра по 2 ГГц");
-				$("#amount-span").text("ядра");
-				break;
+					$("#amount").val(ui.value + " ядра по 2 ГГц");
+					$("#amount-span").text("ядра");
+					break;
 				case 4:
-				$("#amount").val(ui.value + " ядра по 2 ГГц");
-				$("#amount-span").text("ядра");
-				break;
+					$("#amount").val(ui.value + " ядра по 2 ГГц");
+					$("#amount-span").text("ядра");
+					break;
 				case 5:
 					$("#amount").val(ui.value + " ядер по 2 ГГц");
 					$("#amount-span").text("ядeр");
@@ -100,7 +99,7 @@ $(document).ready( function() {
 			$(this).find('.my-handle').text(ui.value);
 			$('#amount-input').val(ui.value);
 		},
-		change: function( event, ui ) {
+		change: function (event, ui) {
 			$(this).find('.my-handle').text(ui.value);
 		}
 	});
@@ -122,11 +121,12 @@ $(document).ready( function() {
 		max: 32,
 		step: 2,
 		range: "min",
-		slide: function(event, ui) {
-			$("#amount2").val(ui.value  + " Гб");
+		slide: function (event, ui) {
+			$("#amount-input2").val(ui.value);
+			$("#amount2").val(ui.value + " Гб");
 			$(this).find('.my-handle').text(ui.value);
 		},
-		change: function( event, ui ) {
+		change: function (event, ui) {
 			$(this).find('.my-handle').text(ui.value);
 		}
 	});
@@ -147,32 +147,85 @@ $(document).ready( function() {
 		max: 500,
 		step: 50,
 		range: "min",
-		slide: function(event, ui) {
-			$("#amount3").val(ui.value  + " Гб");
+		slide: function (event, ui) {
+			$("#amount-input3").val(ui.value);
+			$("#amount3").val(ui.value + " Гб");
 			$(this).find('.my-handle').text(ui.value);
 
 		},
-		create: function( event, ui ) {
-			$('.slider span').append('<div class="my-handle"></div>');
+		change: function (event, ui) {
+			$(this).find('.my-handle').text(ui.value);
 		}
 	});
-
 
 	$("#amount3").val($("#slider-its-configuration3").slider("value").toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ') + " Гб");
 	setTimeout(function () {
 		$("#slider-its-configuration3 .my-handle").text($("#slider-its-configuration3").slider("value"));
 	}, 300);
+	$("#amount-input3").val($("#slider-its-configuration3").slider("value"));
+	$("#amount-input3").keyup(function () {
+		$("#slider-its-configuration3").slider("value", $(this).val());
+		$('#amount-input3').val($(this).val());
+	});
+	// слайдеры в configure-the-settings.html АДРЕСА IPV
+	var configurationIPvCounter = 0;
+	$(".slider-its-configurationIPv").each(function () {
+		$(this).slider({
+			value: 30,
+			min: 1,
+			max: 254,
+			step: 1,
+			range: "min",
+			slide: function (event, ui) {
+				$(this).parents('.rent-VPS-VDS-servers-its-configuration__sliders-item').find('.amount input').val(ui.value);
+				$(this).parents('.rent-VPS-VDS-servers-its-configuration__sliders-item').find('.amount span').val(ui.value + " Шт");
+				$(this).find('.my-handle').text(ui.value);
+
+			},
+			change: function (event, ui) {
+				$(this).find('.my-handle').text(ui.value);
+			},
+			create: function (event, ui) {
+				console.log($(".slider-its-configurationIPv").length);
+
+				if(configurationIPvCounter === $(".slider-its-configurationIPv").length - 1) {
+					$('.slider span').append('<div class="my-handle"></div>');
+				}
+				configurationIPvCounter++;
+			}
+		});
+	});
+
+	$("#amount4").val($("#slider-its-configuration4").slider("value").toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ') + " Шт");
+	setTimeout(function () {
+		$("#slider-its-configuration4 .my-handle").text($("#slider-its-configuration4").slider("value"));
+	}, 300);
+	$("#amount-input4").val($("#slider-its-configuration4").slider("value"));
+	$("#amount-input4").keyup(function () {
+		$("#slider-its-configuration4").slider("value", $(this).val());
+		$('#amount-input4').val($(this).val());
+	});
+
+	$("#amount5").val($("#slider-its-configuration5").slider("value").toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ') + " Шт");
+	setTimeout(function () {
+		$("#slider-its-configuration5 .my-handle").text($("#slider-its-configuration5").slider("value"));
+	}, 300);
+	$("#amount-input5").val($("#slider-its-configuration5").slider("value"));
+	$("#amount-input5").keyup(function () {
+		$("#slider-its-configuration5").slider("value", $(this).val());
+		$('#amount-input5').val($(this).val());
+	});
 	// вызов tabs
 	tabs({
-		btn:'.tabs-items-wrap .tabs-item',
-		tabsBody:'.tabs-wrap',
-		classBody:'active',
+		btn: '.tabs-items-wrap .tabs-item',
+		tabsBody: '.tabs-wrap',
+		classBody: 'active',
 		classBtn: 'active'
 	});
 	tabs({
-		btn:'.FAQ-tab-items .FAQ-tab-item',
-		tabsBody:'.FAQ-tabs-wrap',
-		classBody:'active',
+		btn: '.FAQ-tab-items .FAQ-tab-item',
+		tabsBody: '.FAQ-tabs-wrap',
+		classBody: 'active',
 		classBtn: 'active'
 	});
 	// Активация popover
@@ -180,76 +233,76 @@ $(document).ready( function() {
 
 	if (get_name_browser() == "Safari") {
 
-		if(window.matchMedia("(min-width: 768px)").matches) {
+		if (window.matchMedia("(min-width: 768px)").matches) {
 			$(".FAQ .FAQ-tabs-wrap").css("min-height", "578px");
 		}
-		if(window.matchMedia("(max-width: 768px)").matches) {
+		if (window.matchMedia("(max-width: 768px)").matches) {
 			$(".FAQ .FAQ-tabs-wrap").css("min-height", "100%");
 		}
 	}
 
-    // для инициализации tooltips
-    // $( document ).tooltip({
-    //   track: true
-    // });
+	// для инициализации tooltips
+	// $( document ).tooltip({
+	//   track: true
+	// });
 
-    // скролл по ссылке с атрибутом href
-    // $(".header_nav a[href*='#']").on("click", function(e) {
-    //     e.preventDefault();
-    //     var anchor = $(this);
-    //     $('html, body').stop().animate({
-    //         scrollTop: $(anchor.attr('href')).offset().top
-    //     }, 500);
-    //     return false;
-    // });
+	// скролл по ссылке с атрибутом href
+	// $(".header_nav a[href*='#']").on("click", function(e) {
+	//     e.preventDefault();
+	//     var anchor = $(this);
+	//     $('html, body').stop().animate({
+	//         scrollTop: $(anchor.attr('href')).offset().top
+	//     }, 500);
+	//     return false;
+	// });
 
-    // Скролл по классу .scroll_to и атрибуту data-scroll у кнопки к примеру (data-scroll="куда скроллим" в элементе куда скроллим ставим id потом впишем в куда скроллим)
-    // $(".scroll_to").on("clcik", function(e) {
-    //     e.preventDefault();
-    //     var anchor = $(this);
-    //     $('html, body').stop().animate({
-    //         scrollTop: $("#" + anchor.data('scroll')).offset().top
-    //     }, 500);
-    //     return false;
-    // });
+	// Скролл по классу .scroll_to и атрибуту data-scroll у кнопки к примеру (data-scroll="куда скроллим" в элементе куда скроллим ставим id потом впишем в куда скроллим)
+	// $(".scroll_to").on("clcik", function(e) {
+	//     e.preventDefault();
+	//     var anchor = $(this);
+	//     $('html, body').stop().animate({
+	//         scrollTop: $("#" + anchor.data('scroll')).offset().top
+	//     }, 500);
+	//     return false;
+	// });
 
-    //  Активация слайдера
-    // $(".owl-carousel").owlCarousel({
-    //     loop: true,
-    //     items: 1,
-    //     dots: true
-    // });
+	//  Активация слайдера
+	// $(".owl-carousel").owlCarousel({
+	//     loop: true,
+	//     items: 1,
+	//     dots: true
+	// });
 
-    // Кастомные кнопки управления слайдером
-    // var owl = $('.owl-carousel');
-    // owl.owlCarousel();
-    // // Go to the next item
-    // $('.customNextBtn').click(function() {
-    //     owl.trigger('next.owl.carousel', [700]);
-    // });
-    // // Go to the previous item
-    // $('.customPrevBtn').click(function() {
-    //     // With optional speed parameter
-    //     // Parameters has to be in square bracket '[]'
-    //     owl.trigger('prev.owl.carousel', [700]);
-    // });
+	// Кастомные кнопки управления слайдером
+	// var owl = $('.owl-carousel');
+	// owl.owlCarousel();
+	// // Go to the next item
+	// $('.customNextBtn').click(function() {
+	//     owl.trigger('next.owl.carousel', [700]);
+	// });
+	// // Go to the previous item
+	// $('.customPrevBtn').click(function() {
+	//     // With optional speed parameter
+	//     // Parameters has to be in square bracket '[]'
+	//     owl.trigger('prev.owl.carousel', [700]);
+	// });
 
 
 });
 
-$(window).resize(function() {
+$(window).resize(function () {
 	if (get_name_browser() == "Safari") {
 
-		if(window.matchMedia("(min-width: 768px)").matches) {
+		if (window.matchMedia("(min-width: 768px)").matches) {
 			$(".FAQ .FAQ-tabs-wrap").css("height", "578px");
 		}
-		if(window.matchMedia("(max-width: 768px)").matches) {
+		if (window.matchMedia("(max-width: 768px)").matches) {
 			$(".FAQ .FAQ-tabs-wrap").css("height", "inherit");
 		}
 	}
 });
 
-$(window).scroll(function() {
+$(window).scroll(function () {
 
 });
 
